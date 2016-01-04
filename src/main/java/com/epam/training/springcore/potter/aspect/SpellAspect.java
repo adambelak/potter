7c3401b061d55014960198d34b8fa181611495d1
Adam Belak <adam_belak@epam.com>
2016-01-04 08:35:49 +0100

    Change SpellAspect

--- src/main/java/com/epam/training/springcore/potter/aspect/SpellAspect.java
+++ src/main/java/com/epam/training/springcore/potter/aspect/SpellAspect.java
@@ -1,13 +1,10 @@
 package com.epam.training.springcore.potter.aspect;
 
 import com.epam.training.springcore.potter.domain.Wizard;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Optional;
 
 @Aspect
 public class SpellAspect {
@@ -19,46 +16,29 @@ public class SpellAspect {
     private static final String EXPELLIARMUS = "expelliarmus, {}";
     private static final String SALVIO_HEXIA = "salvio hexia, {}";
 
-
-    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.CruciatusCurse.spell(..))")
-    public void beforeCruciatusSpell(JoinPoint joinPoint) {
-        logSpell(joinPoint, CRUCIO);
+    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.CruciatusCurse.spell(..)) && args(wizard)")
+    public void beforeCruciatusSpell(Wizard wizard) {
+        LOGGER.trace(CRUCIO, wizard);
     }
 
-    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.DisarmingCharm.spell(..))")
-    public void beforeDisarmingCharm(JoinPoint joinPoint) {
-        logSpell(joinPoint, EXPELLIARMUS);
+    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.DisarmingCharm.spell(..)) && args(wizard)")
+    public void beforeDisarmingCharm(Wizard wizard) {
+        LOGGER.trace(EXPELLIARMUS, wizard);
     }
 
-    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.KillingCurse.spell(..))")
-    public void beforeKillingCurse(JoinPoint joinPoint) {
-        logSpell(joinPoint, AVADA_KEDAVRA);
+    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.KillingCurse.spell(..)) && args(wizard)")
+    public void beforeKillingCurse(Wizard wizard) {
+        LOGGER.trace(AVADA_KEDAVRA, wizard);
     }
 
-    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.SalvioHexia.spell(..))")
-    public void beforeSalvioHexia(JoinPoint joinPoint) {
-        logSpell(joinPoint, SALVIO_HEXIA);
+    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.SalvioHexia.spell(..)) && args(wizard)")
+    public void beforeSalvioHexia(Wizard wizard) {
+        LOGGER.trace(SALVIO_HEXIA, wizard);
     }
 
-    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.SummoningCharm.spell(..))")
-    public void beforeSummoningCharm(JoinPoint joinPoint) {
-        logSpell(joinPoint, ACCIO);
-    }
-
-    private void logSpell(JoinPoint joinPoint, String template) {
-        Optional<Wizard> wizard = getWizard(joinPoint);
-        if (wizard.isPresent()) {
-            LOGGER.trace(template, wizard.get());
-        }
-    }
-
-    private Optional<Wizard> getWizard(JoinPoint joinPoint) {
-        Optional<Wizard> result = Optional.empty();
-        Object[] args = joinPoint.getArgs();
-        if (args.length > 0) {
-            result = Optional.of((Wizard) args[0]);
-        }
-        return result;
+    @Before("execution(void com.epam.training.springcore.potter.intern.domain.spell.SummoningCharm.spell(..)) && args(wizard)")
+    public void beforeSummoningCharm(Wizard wizard) {
+        LOGGER.trace(ACCIO, wizard);
     }
 
 }
